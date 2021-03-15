588a0ba1b8d1102a095d65f22fbb84743f4196bf
Nguyễn Ngọc Chinh <chinhnguyen.210399@gmail.com>
2021-03-15 15:19:33 +0700

    Add new page admin

---
+++ frontend/layouts/admin.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-app id="inspire">
+        <Header v-model="expandOnHover"></Header>
+        <Sidebar :expand-on-hover.sync="expandOnHover"></Sidebar>
+        <v-main>
+            <v-container fluid>
+                <nuxt />
+            </v-container>
+        </v-main>
+        <Footer />
+    </v-app>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+import Sidebar from '@/components/SideBar/Admin'
+import Header from '@/components/Header/Admin'
+import Footer from '@/components/Footer/Admin'
+export default {
+    name: 'Layout',
+
+    components: {
+        Footer,
+        Sidebar,
+        Header,
+    },
+
+    props: {
+        source: String,
+    },
+    data: () => ({
+        expandOnHover: false,
+    }),
+    computed: {
+        ...mapState(['Customizer_drawer']),
+    },
+
+    methods: {
+        ...mapMutations({
+            setCustomizerDrawer: 'SET_CUSTOMIZER_DRAWER',
+        }),
+    },
+}
+</script>
---
+++ frontend/pages/admin/users/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>USERS</h1>
+</template>
+
+<script>
+export default {
+    layout: 'admin',
+}
+</script>
