2f51d62a970368254650ee5767d9c0fdb50548bb
Your Name <you@example.com>
2021-03-23 22:38:44 +0700

    hoan thanh click nha dang hot

--- frontend/pages/baidang/hot/index.vue
+++ frontend/pages/baidang/hot/index.vue
@@ -1,19 +1,24 @@
 <template>
-    <v-container>
+    <v-container class="">
+        <h1>Hot</h1>
         <v-row>
-            <v-col cols="4">
-                <FormBaiDang />
+            <v-col cols="4" class="flex-grow-0 flex-shrink-0 mt-3">
+                <v-card class="pa-2" outlined tile> </v-card>
             </v-col>
-            <v-col cols="8">
-                <KQHienThi></KQHienThi>
+            <v-col cols="8" style="min-width: 100px; max-width: 100%" class="flex-grow-1 flex-shrink-0">
+                <v-card class="pa-2" outlined tile> I'm 1 column wide and I grow to take all the space </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="4" class="flex-grow-0 mt-3">
+                <v-card class="pa-2" outlined tile> I'm 2 column wide </v-card>
             </v-col>
         </v-row>
     </v-container>
 </template>
+
 <script>
-import KQHienThi from '~/components/DSBaiDang/KQHienThi'
-import FormBaiDang from '~/components/DSBaiDang/FormBaiDang'
-export default {
-    components: { KQHienThi, FormBaiDang },
-}
+export default {}
 </script>
+
+<style></style>
