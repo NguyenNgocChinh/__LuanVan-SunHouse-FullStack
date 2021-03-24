2223f0821c219e1b940653ef0cc07eebf501136e
tranthingocnhat <Dh51705101@student.stu.edu.vn>
2021-03-24 18:39:56 +0700

    Nhat chia component, chi tiet san pham chua hoan thanh

--- frontend/pages/baidang/hot/index.vue
+++ frontend/pages/baidang/hot/index.vue
@@ -1,24 +1,27 @@
 <template>
-    <v-container class="">
-        <h1>Hot</h1>
-        <v-row>
-            <v-col cols="4" class="flex-grow-0 flex-shrink-0 mt-3">
-                <v-card class="pa-2" outlined tile> </v-card>
-            </v-col>
-            <v-col cols="8" style="min-width: 100px; max-width: 100%" class="flex-grow-1 flex-shrink-0">
-                <v-card class="pa-2" outlined tile> I'm 1 column wide and I grow to take all the space </v-card>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="4" class="flex-grow-0 mt-3">
-                <v-card class="pa-2" outlined tile> I'm 2 column wide </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+    <KQHienThi :baidangs="baidangs" />
 </template>
 
 <script>
-export default {}
+import KQHienThi from '~/components/DSBaiDang/KQHienThi'
+export default {
+    components: { KQHienThi },
+    data() {
+        return {
+            baidangs: null,
+        }
+    },
+    created() {
+        this.getBaiDangHot()
+    },
+    methods: {
+        async getBaiDangHot() {
+            const result = await this.$axios.$get(
+                `https://api.sunhouse.stuesports.info/api/baidang/hot?page=${this.page}`
+            )
+            this.baidangs = result
+            // this.baidanghots_loading = false
+        },
+    },
+}
 </script>
-
-<style></style>
