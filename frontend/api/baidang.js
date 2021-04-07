69f6c74412bfeafe702808da47eabaab7cc9df6b
NguyenNgocChinh <chinhnguyen.210399@gmail.com>
2021-04-07 11:53:49 +0700

    update to testing

--- frontend/api/baidang.js
+++ frontend/api/baidang.js
@@ -3,8 +3,8 @@ const ENV = {
     chothue: 'https://api.sunhouse.stuesports.info/api/baidang/chothue',
     raoban: 'https://api.sunhouse.stuesports.info/api/baidang/raoban',
     hot: 'https://api.sunhouse.stuesports.info/api/baidang/hot',
-    choduyet: 'https://api.sunhouse.stuesports.info/api/baidang/choduyet',
-    duyetbai: 'https://api.sunhouse.stuesports.info/api/baidang/duyetbai', // NOT INCLUDE /
+    choduyet: 'http://localhost:8000/api/baidang/choduyet',
+    duyetbai: 'http://localhost:8000/api/baidang/duyetbai', // NOT INCLUDE /
     delete: 'https://api.sunhouse.stuesports.info/api/baidang/',
 }
 export default ENV
