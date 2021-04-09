f03aff12e50be0d6dd2cfc5e4ba8ddf0c5818efe
NguyenNgocChinh <chinhnguyen.210399@gmail.com>
2021-04-09 09:10:48 +0700

    change api for testting

--- frontend/api/baidang.js
+++ frontend/api/baidang.js
@@ -3,7 +3,7 @@ const ENV = {
     chothue: 'https://api.sunhouse.stuesports.info/api/baidang/chothue',
     raoban: 'https://api.sunhouse.stuesports.info/api/baidang/raoban',
     hot: 'https://api.sunhouse.stuesports.info/api/baidang/hot',
-    choduyet: 'http://localhost:8000/api/baidang/choduyet',
+    choduyet: 'http://localhost:8000/api/baidang/choduyet/',
     duyetbai: 'http://localhost:8000/api/baidang/duyetbai', // NOT INCLUDE /
     delete: 'https://api.sunhouse.stuesports.info/api/baidang/',
 }
